8fdd0e77f8eb9d37aee9750a15c61f626ab609fb
Anton <Smirnov>
2021-08-09 18:21:46 +0300

    Initial Commit

---
+++ Regions-ApplicationTests/Regions_ApplicationTests.swift
@@ -0,0 +1,33 @@
+//
+//  Regions_ApplicationTests.swift
+//  Regions-ApplicationTests
+//
+//  Created by Anton Smirnov on 09.08.2021.
+//
+
+import XCTest
+@testable import Regions_Application
+
+class Regions_ApplicationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
