8b42bf71465004a3c772206fad2e89e8561e6fec
vaskocuturilo <vaskocuturilo@rambler.ru>
2021-08-17 09:31:43 +0300

    Resolve conflict

--- Regions-Application/View/CameraViewController.swift
+++ Regions-Application/View/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+<<<<<<< Updated upstream
 class CameraViewController: UIViewController, UIGestureRecognizerDelegate {
     
     override func viewDidLoad() {
@@ -21,4 +22,25 @@ class CameraViewController: UIViewController, UIGestureRecognizerDelegate {
         navigationItem.leftBarButtonItem = backBTN
         navigationController?.interactivePopGestureRecognizer?.delegate = self
     }
+=======
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+>>>>>>> Stashed changes
 }
